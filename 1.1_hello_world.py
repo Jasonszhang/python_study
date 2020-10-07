5579c26dd106fac4eddde888d1808001dfaf59a2
jasonszhang <1368854098@qq.com>
2020-10-07 22:35:39 +0800

    linux

---
+++ 1.1_hello_world.py
@@ -0,0 +1,4 @@
+#this is my first python file
+print("hello world,nice to meet you Python")
+print("simple is better than complex")
+print("-----------------------------")
---
+++ 1.6_annotation.py
@@ -0,0 +1,20 @@
+#this is an annotation for single line
+
+"""
+this is an annotation for multiple line
+second line
+third line
+这几行将会被解释器忽略
+"""
+
+'''
+this is another way to annotate
+second line
+third linw
+'''
+
+#单行注释
+#print("解释器将会忽略此行")
+print("此行不会被忽略")  #简单的内容可以放在语句后面
+
+# 快捷键为Ctrl+/
