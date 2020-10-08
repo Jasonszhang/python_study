d9ab2bea87bb0a69108edb8f3d2436e9680c67eb
jasonszhang <1368854098@qq.com>
2020-10-08 12:26:25 +0800

    linux

--- 1.6_annotation.py
+++ 1.6_annotation.py
@@ -1,7 +1,7 @@
-#注释
+#注释+转义字符
 #2020.10.7
-#this is an annotation for single line
 
+#this is an annotation for single line
 """
 this is an annotation for multiple line
 second line
@@ -12,7 +12,7 @@ third line
 '''
 this is another way to annotate
 second line
-third linw
+third line
 '''
 
 #单行注释
@@ -20,3 +20,13 @@ third linw
 print("此行不会被忽略")  #简单的内容可以放在语句后面
 
 # 快捷键为Ctrl+/
+
+#转义字符 Escape character 
+# \n 换行
+# \t 制表符，（4个空格）
+print("-----------------")
+print("输出的内容",end="\n")  # end="\n"换行结束符，print()默认换行
+print("输出的内容",end=" >>>这个是结束符号\n")
+print("hello\nPython")      # \n转义
+print("\tabcd")             # \t制表符
+
