0e0df78fc005eeaa0a51e4265276c6b95983b370
jasonszhang <1368854098@qq.com>
2020-10-20 22:18:00 +0800

    linux

---
+++ 1.7_operator.py
@@ -0,0 +1,49 @@
+# 运算符
+# 2020.10.20
+
+#算数运算法
+
+'''
++ - * /  //  % ** ()
+'''
+
+print(2**3)
+print(9//4)
+print(9%4)
+
+#赋值运算符
+
+# = 
+
+a = 1
+b = 100
+c = a
+a = b
+b = c
+print(f"交换后的a为{a}")
+print(f"交换后的b为{b}")
+
+num1, str1, float1 = 3, 'jjj', 0.23
+print(num1)
+print(str1)
+print(float1)
+
+a=b=c=d=100
+print(a,b,c,d)
+
+#符合赋值运算符
+a = 1
+a += 1
+print(a)
+
+#比较运算符
+"""
+==
+!=
+>
+<
+>=
+<=
+"""
+#逻辑运算符
+# and or not
